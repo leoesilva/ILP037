2668252a2ad7deb8c856957c3bb836548a9d4401
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2024-08-19 22:42:07 -0300

    feat: AtacarSozinho
    Rascunho da lógica de ataque

--- JogoGuerra/src/Simulador/AtacarSozinho.java
+++ JogoGuerra/src/Simulador/AtacarSozinho.java
@@ -1,11 +1,45 @@
 package Simulador;
 
-public class AtacarSozinho implements Estrategia {
+import java.util.Random;
+import java.util.Scanner;
 
+public class AtacarSozinho implements Estrategia {
+    Scanner input = new Scanner(System.in);
     @Override
     public void agir(Pais adversario) {
+        int qtdInicialSoldados = adversario.getQtdSoldados();
+        if(plantarEvidenciasFalsas()) {
+            System.out.println("Evidência falsa plantada com sucesso!");
+            while (adversario.getQtdSoldados() > (qtdInicialSoldados * 0.3)) {
+                soltarBombas();
+            }
+            derrubarGoverno();
+        } else {
+            System.out.println("Evidência falsa falhou. Deseja atacar mesmo assim? (S/N): ");
+            char desejaAtacar = input.next().charAt(0);
+            if(desejaAtacar == 'S') {
+                while (adversario.getQtdSoldados() > (qtdInicialSoldados * 0.3)) {
+                    soltarBombas();
+                }
+            } else {
+                System.out.println("Aguardando ação...");
+            }
+        }
+    }
+
+    private void derrubarGoverno() {
         // TODO Auto-generated method stub
-        
+        throw new UnsupportedOperationException("Unimplemented method 'derrubarGoverno'");
+    }
+
+    private void soltarBombas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'soltarBombas'");
+    }
+
+    private boolean plantarEvidenciasFalsas() {
+        Random tentativa = new Random();
+        return tentativa.nextBoolean();
     }
 
 }
